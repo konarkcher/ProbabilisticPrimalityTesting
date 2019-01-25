f70904afebebba9a8b31a4ba0d17105b65a15ee9
Konarkcher <konarkcher@yandex.ru>
2019-01-25 19:44:15 +0300

    Add experiments and Riemann hypothesis

--- ECPP_Rabin-Miller/ecpp/brute_force.py
+++ ECPP_Rabin-Miller/ecpp/brute_force.py
@@ -1,7 +1,8 @@
 from math import sqrt
+import time
 
 
-def trial_division(n):
+def trial_division(n, timer=0):
     """
     Trial division algorithm
     Args:
@@ -10,7 +11,11 @@ def trial_division(n):
     Returns:
         If n is prime
     """
+    start = time.time()
     for i in range(2, int(sqrt(n)) + 1):
         if n % i == 0:
             return False
+        if timer > 0:
+            if time.time() - start > timer:
+                return 'too long'
     return True
